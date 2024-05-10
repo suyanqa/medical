eb0293a493e299032f14e336a029e5bce8a6e539
suyanqa <suyan1254088@gmail.com>
2024-05-10 15:35:57 +0800

    suyanqa
    配置了后端解决跨域问题,允许来自前端3000端口的请求.

--- src/main/java/com/suyanqa/medical/MedicalApplication.java
+++ src/main/java/com/suyanqa/medical/MedicalApplication.java
@@ -2,8 +2,10 @@ package com.suyanqa.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 public class MedicalApplication {
 
     public static void main(String[] args) {
